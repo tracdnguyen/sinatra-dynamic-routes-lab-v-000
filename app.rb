e20c5c0207ac3829a01b5d0953e0b8e2b6d670d8
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-15 21:04:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,8 +4,9 @@ class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
     @reverse_name = params[:name].reverse
-
-
   end
 
+  get '/square/:number' do 
+    @squared_number = params[:number].to_i * params[:number].to_i *
+  end
 end
