b05857818337746e9e7eb8f4c9d51587d715d7c7
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-15 21:01:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    @reverse_name = params[:name]
+    @reverse_name = params[:name].reverse
     
     
   end
